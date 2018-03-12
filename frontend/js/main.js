b8e81142958ab2f1caa7f6c3b72ac6bafcc4849a
WGS\shornsey <shornsey@wenona.nsw.edu.au>
2018-03-12 12:19:41 +1100

    Added integer validation

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,16 @@
 function get_data() {
   var rand = document.getElementById('value').value;
 
+  // Check that value is integer
+  if (!Number.isInteger(parseFloat(rand))) {
+    console.log("Not Int!")
+    document.getElementById('alert').style.display = "block";
+    return;
+  } else {
+    document.getElementById('alert').style.display = "none";
+  }
+
+  // Fetch from api
   fetch('/api/' + rand).then((response) => {
     return response.json()
   }).then((json) => {
